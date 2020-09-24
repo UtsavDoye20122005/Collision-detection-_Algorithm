cbd22dc530d0f371d29dceda96a90aceaa85fb16
UtsavDoye20122005 <71711535+UtsavDoye20122005@users.noreply.github.com>
2020-09-24 18:57:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var a,b
+
+function setup() {
+  createCanvas(800,400);
+  a= createSprite(400, 200, 50, 50);
+  b= createSprite(350,150,50,45);
+}
+
+function draw() {
+  background(255,255,255);  
+  
+  a.x=mouseX;
+  a.y=mouseY;
+  a.shapeColor="Black";
+  b.shapeColor="Blue"
+
+  if(a.x-b.x<a.width/2+b.width/2 &&
+    b.x-a.x<a.width/2+b.width/2 &&
+    a.y-b.y<a.height/2+b.height/2 &&
+    b.y-a.y<a.height/2+b.height/2){
+  
+    a.shapeColor="Purple";
+    b.shapeColor="Green";
+  
+  } 
+  else{
+
+a.shapeColor="Black";
+b.shapeColor="Blue";
+
+
+  }
+
+    drawSprites();
+  
+}
